053ec84c5f86b8ae462dd3609c3cb51cf51d3b17
Gabriel Giri <gabrielgiri@gmail.com>
2019-07-03 10:47:04 -0300

    Bake a tester by Fernando

--- src/component/bake-a-tester.js
+++ src/component/bake-a-tester.js
@@ -30,16 +30,14 @@ const BakeTester = (props) => (
                     </div>
 
 
-            <p> Our  idea is develop a place where anyone can learn how to become a tester.
-
-            In that place you could find all definitions and tips to do the best testing.</p>
-
-            <p>Technical and exercises in a same place to familiarize the audience with the terminology used in the testing work.</p>
-
-            <p> Our purpose is to extend it to be used by different communities.
-
-             Develop an interdisciplinary application for helping young professionals becoming testers through some kind of introductory lab. </p>
-
+            <p>Bake A Tester 3.0 is a website developed by a team both of testers and developers from Rosario Endava Delivery Unit. The main idea behind this project is to provide a place from where newcomers and people outside the testing environment can learn how to become testers.</p>
+            <p>But just between us, it's much more than that.</p>
+            <p>It's intended to be an internal DU's project to be maintained by testers and developers with the objective of sharing their experiences and dive deeper in each others' areas of expertise, in a way that will enlight testers with developers' knowledge and viceversa.</p>
+            <p>The platform serves as a point of contact cross-roles and cross-disciplines, where anyone can find information related to Management Discipline, Automation Discipline, Performance Discipline, among many others. </p>
+            <p>You will find useful links, definitions and tips on how to become a better tester, certifications information, and so on. Furthermore, technical excercises are offered so you familiarize with testing terminology.</p>
+            <p>As members of interdisciplinary teams it is within our strongest beliefs that all as a whole are responsible for ensuring the highest quality-standards of the products we deliver. Come on board. Be more!</p>
+            <p>When you hear the bell you'll know your Tester is fully baked and ready to come out the oven!</p>
+            <p>We wish you a good baking! ;)</p>
 
             <hr />
                    </main>
@@ -59,18 +57,22 @@ const BakeTester = (props) => (
 
                                 </div>
                    <ul>
-                      <li>Our purpose of this training is share and impart knowledge.</li>
-                      <li>To familiarize the audience with the terminology used in the testing work.</li>
-                      <li>To arrive at a common understanding of the scope of testing activity.</li>
-                      <li>Execute a program with the intent of finding errors to:</li>
-                                <ul>
-                                    <li>Determine whether system meets specifications</li>
-                                    <li>Determine whether system meets user’s needs</li>
-                                </ul>
-                       <li>Understand about techniques for planning tests.</li>
-                       <li>Apply techniques of software testing in projects.</li>
-                       <li>Select the appropriate testing techniques and objectives.</li>
-                       <li>Continuously improve the testing process.</li>
+                      <li>Share and impart knowledge</li>
+                      <li>Be a trustworthy point of contact for every role in Endava</li>
+                      <li>Provide a point of contact for multiple disciplines</li>
+                      <li>Standardize the level of knowledge Endava is to ensure their customer we us professionals are ready to deliver</li>
+                      <li>Familiarize the audience with testing-related terminology</li>
+                      <li>Arrive at a common understanding of the scope of testing activity</li>
+                      <li>Understand about techniques for planning tests</li>
+                      <li>Apply techniques of software testing in projects</li>
+                      <li>Select the appropriate testing techniques and define its objectives</li>
+                      <li>Continuously improve the testing process</li>
+                      <li>Test yourself from what you have learnt through the proposed excercises to:</li>
+                        <ul>
+                            <li>Determine whether system meets specifications</li>
+                            <li>Determine whether system meets user’s needs</li>
+                        </ul>
+                      <li>Learn together by continuosly improving this interdisciplinary platform</li>
                     </ul>
 
 
