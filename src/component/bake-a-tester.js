ffcf9b3c87476cd6e1114f65c512f58a0b75445d
Gabriel Giri <gabrielgiri@gmail.com>
2019-07-04 14:35:02 -0300

    Added image to bake a tester

--- src/component/bake-a-tester.js
+++ src/component/bake-a-tester.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import bake from '../images/bakeAtester.png';
 
 const BakeTester = (props) => (
 
@@ -26,77 +25,17 @@ const BakeTester = (props) => (
                             <span>r</span>
                         </div>
                     </div>
-
-            <p>Bake A Tester 3.0 is a website developed by a team both of testers and developers from Rosario Endava Delivery Unit. The main idea behind this project is to provide a place from where newcomers and people outside the testing environment can learn how to become testers.</p>
-            <p>But just between us, it's much more than that.</p>
-            <p>It's intended to be an internal DU's project to be maintained by testers and developers with the objective of sharing their experiences and dive deeper in each others' areas of expertise, in a way that will enlight testers with developers' knowledge and viceversa.</p>
-            <p>The platform serves as a point of contact cross-roles and cross-disciplines, where anyone can find information related to Management Discipline, Automation Discipline, Performance Discipline, among many others. </p>
-            <p>You will find useful links, definitions and tips on how to become a better tester, certifications information, and so on. Furthermore, technical excercises are offered so you familiarize with testing terminology.</p>
-            <p>As members of interdisciplinary teams it is within our strongest beliefs that all as a whole are responsible for ensuring the highest quality-standards of the products we deliver. Come on board. Be more!</p>
-            <p>When you hear the bell you'll know your Tester is fully baked and ready to come out the oven!</p>
-            <p className="mb-2"><strong>We wish you a good baking! ;)</strong></p>
+                    <img src={bake} className="mb-5"></img>
+                    <p><strong>Bake A Tester 3.0</strong> is a website developed by a team both of testers and developers from Rosario Endava Delivery Unit. The main idea behind this project is to provide a place from where newcomers and people outside the testing environment can learn how to become testers.</p>
+                    <p>But just between us, it's much more than that.</p>
+                    <p>It's intended to be an internal DU's project to be maintained by testers and developers with the objective of sharing their experiences and dive deeper in each others' areas of expertise, in a way that will enlight testers with developers' knowledge and viceversa.</p>
+                    <p>The platform serves as a point of contact cross-roles and cross-disciplines, where anyone can find information related to Management Discipline, Automation Discipline, Performance Discipline, among many others. </p>
+                    <p>You will find useful links, definitions and tips on how to become a better tester, certifications information, and so on. Furthermore, technical excercises are offered so you familiarize with testing terminology.</p>
+                    <p>As members of interdisciplinary teams it is within our strongest beliefs that all as a whole are responsible for ensuring the highest quality-standards of the products we deliver. Come on board. Be more!</p>
+                    <p>When you hear the bell you'll know your Tester is fully baked and ready to come out the oven!</p>
+                    <p className="mb-2"><strong>We wish you a good baking! ;)</strong></p>
             <hr />
                    </main>
-                            
-                   {/*
-                  
-                  
-                            <div className="typewriter rounded">
-
-                                    <div className="animate title">
-
-                                        <span>o</span>
-                                        <span>u</span>
-                                        <span>r</span>&nbsp;
-                                        <span>g</span>
-                                        <span>o</span>
-                                        <span>a</span>
-                                        <span>l</span>
-                                        <span>s</span>
-                                      </div>
-
-                                </div>
-                   <ul>
-                      <li>Share and impart knowledge</li>
-                      <li>Be a trustworthy point of contact for every role in Endava</li>
-                      <li>Provide a point of contact for multiple disciplines</li>
-                      <li>Standardize the level of knowledge Endava is to ensure their customer we us professionals are ready to deliver</li>
-                      <li>Familiarize the audience with testing-related terminology</li>
-                      <li>Arrive at a common understanding of the scope of testing activity</li>
-                      <li>Understand about techniques for planning tests</li>
-                      <li>Apply techniques of software testing in projects</li>
-                      <li>Select the appropriate testing techniques and define its objectives</li>
-                      <li>Continuously improve the testing process</li>
-                      <li>Test yourself from what you have learnt through the proposed excercises to:</li>
-                        <ul>
-                            <li>Determine whether system meets specifications</li>
-                            <li>Determine whether system meets user’s needs</li>
-                        </ul>
-                      <li>Learn together by continuosly improving this interdisciplinary platform</li>
-                    </ul>
- <div className="typewriter rounded">
-
-                        <div className="animate title">
-
-                            <span>t</span>
-                            <span>o</span>
-                            <span>p</span>
-                            <span>i</span>
-                            <span>c</span>
-                            <span>s</span>&nbsp;
-                            <span>t</span>
-                            <span>o</span>&nbsp;
-                            <span>c</span>
-                            <span>o</span>
-                            <span>v</span>
-                            <span>e</span>
-                            <span>r</span>
-                        </div>
-
-                    </div>
-
-
-*/}
            <h2 class="mt-1 mb-3">Topics Cover</h2>     
 
         <div class="accordion-option">
