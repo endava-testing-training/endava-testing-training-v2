d4e924b5b4af0801bf2c8690026aafce303f905c
Ernesto Bordenave <ernesto.bordenave@endava.com>
2019-07-04 11:12:52 -0300

    :Communities

--- src/component/communities.js
+++ src/component/communities.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
  const Communities = (props) => (
-
                    <div className="container mt-5">
-
                        <main className="page-content" aria-label="Content">
                            <div className="wrapper">
                                <div className="home">
-
-                                   <div className="typewriter rounded">
+                                    <div className="typewriter rounded">
                                        <h2>{props.title}</h2>
                                        <div className="animate title">
-
                                            <span>c</span>
                                            <span>o</span>
                                            <span>m</span>
@@ -23,25 +19,81 @@ import React from 'react';
                                            <span>i</span>
                                            <span>e</span>
                                            <span>s</span>
-
                                        </div>
-                                   </div>
-                                   <h2>Management</h2>
+                                    </div>
 
-                                   <hr />
-                                   <h2>Performance</h2>
+                                    <h2>Management</h2>
+                                    <h3>Community target</h3>
+                                    <p>Everyone wanting to improve their testing abilities  through the application of the best practices, processes,
+                                    techniques and available tools to facilitate and improve the quality of their work is invited to join and 
+                                    participate in the community, always in a warm environment of respect and professionalism.</p>
 
-                                   <hr />
-                                   <h2>Automation</h2>
+                                    <h3>Community goals </h3>
+                                    <p>The goals of the Testing Management Community in Rosario are: </p>
+                                    <ul>
+                                        <li>Increase testing discipline expertise in Testing Management </li>
+                                        <li>Participate in practical exercises to put the theory and your knowledge into practice</li>
+                                        <li>Motivation to join Testing Management practice</li>
+                                        <li>Create a learning culture for best practices </li>
+                                        <li>Learn fundamental concepts of the Software Testing  </li>
+                                        <li>Create trainings for the community : Ensure team members from different levels go through the training according to their level.</li>
+                                        <li>Share experiences with the community </li>
+                                        <li>Master the best practices for different testing activities, such as: Test case analysis, Test case creation, Test coverage, Defect creation and management, Test  reporting and more</li>
+                                        <li>Increase our knowledge about available Certifications related to Testing </li>
+                                    </ul>
+                                    <p></p>
+                                    <p className="communityleads">Community lead : Gisela Vivas - Carla Passaro</p>
 
+                                    <hr />
+                                    <h2>Performance Testing </h2>
+                                    <h3>Community target</h3>
+                                    <p>Everyone wanting to improve their performace testing abilities is invited to join and participate in the community, always in an environment of respect and professionality
+                                    Employees starting from Senior technitian.</p>
 
-                               </div>
-                           </div>
+                                    <h3>Community goals</h3>
+                                    <p>The goals of the Performance Community in Rosario are:</p>
+                                    <ul>
+                                        <li>Have a good understanding of the performance testing concepts.</li>
+                                        <li>Identify and use the best performance testing tools in order to make the best out of them, and be able to compare the benefits and drawbacks of each tool.</li>
+                                        <li>Understand the different metrics applicable in Performance testing and the different alternatives to make results change towards the desired path.</li>
+                                        <li>Share all the knowledge with the community and have a common language.</li>
+                                        <li>Create learning culture, for both theory and practice.</li>
+                                        <li>Create trainings for the community.</li>
+                                        <li>Have the ability to apply performance testing to different types of sites, and be able to identify the different metrics suitable in each case.</li>
+                                        <li>Be able to identify alternative solutions to any problem or situation identified during the analysis of performance testing results.</li>
+                                        <li>Be able to integrate performance testing tools to CI tools, such as Jenkins, TeamCity, etc.</li>
+                                    </ul>
+                                    <p></p>
+                                    <p className="communityleads">Community lead: Martín zapata - Maria Alejandra Usart </p>
+                                    <hr />
+                                    <h2>Automation Testing</h2>
+                                    <h3>Community target</h3>
 
-                       </main>
-
-                   </div>
-
-               );
+                                    <p>Every software development group tests its products, yet delivered software always has defects. Test engineers strive to catch them before the product is released but they always believe in and they often reappear, even with the best manual testing processes. Test Automation software is the best way to increase the effectiveness, efficiency and coverage of your software testing.
+                                    If you want to learn about automation practices or you just want to improve your knowledge on it.. Join us :)</p>
+                                    <h3>Community Objectives:</h3>
+                                    <ul>
+                                        <li>Learn and share automation concepts with the community.</li>
+                                        <li>Create trainings.</li>
+                                        <li>Generate new relationships between members of the community.</li>
+                                        <li>Understand the objectives, advantages, disadvantages, and limitations of test automation.</li>
+                                        <li>Identify technical success factors of a test automation project.</li>
+                                        <li>Analyze test automation tools and report technical findings and recommendations.</li>
+                                        <li>Understand "design for testability" and "design for test automation" methods applicable to the SUT.</li>
+                                        <li>Explain the structure of the Generic Test Automation Architecture.</li>
+                                        <li>Apply guidelines that support effective test tool pilot and deployment activities.</li>
+                                        <li>Analyze deployment risks and identify technical issues that could lead to failure of the test automation project, and plan mitigation strategies.</li>
+                                        <li>Classify metrics that can be used to monitor the test automation strategy and effectiveness.</li>
+                                        <li>Apply criteria for determining the suitability of tests for automation.</li>
+                                        <li>Understand the factors in transitioning from manual to automation testing.</li>
+                                        <li>Learn the factors to consider in implementing automated regression testing, new feature testing, and confirmation testing.</li>
+                                    </ul>
+                                    <p></p>
+                                    <p className="communityleads">Community lead: Adelquis Trinidad - Guillermo Cejas</p>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
+                );
 
  export default Communities;
