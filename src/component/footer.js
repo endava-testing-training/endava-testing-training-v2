fc355c9acccada0ef269d5fa35f6b13c87395e97
Gabriel Giri <gabrielgiri@gmail.com>
2019-07-04 11:41:46 -0300

    CSS improvements on home page

--- src/component/footer.js
+++ src/component/footer.js
@@ -10,10 +10,10 @@ const EndavaFooter = () => (
 
             <div className="offset-col-1 col-lg-12 col-md-12 col-sm-12 col-xs-12 inner">
                 <div className="float-right mt-2">
-                        <a href="https://www.facebook.com/endava" rel="me" itemprop="sameAs" aria-label="visit us on " target="_blank" title="facebook"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
-                        <a href="https://twitter.com/endava" rel="me" itemprop="sameAs" aria-label="visit us on " target="_blank" title="twitter"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
-                        <a href="https://www.linkedin.com/company/endava" rel="me" itemprop="sameAs" aria-label="visit us on " target="_blank" title="linkedin"> <FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                        <a href="https://www.instagram.com/endava/" rel="me" itemprop="sameAs" aria-label="visit us on " target="_blank" title="instagram"> <FontAwesomeIcon icon={['fab', 'instagram']} /></a>
+                        <a href="https://www.facebook.com/endava" rel="me" itemProp="sameAs" aria-label="visit us on " target="_blank" title="facebook"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
+                        <a href="https://twitter.com/endava" rel="me" itemProp="sameAs" aria-label="visit us on " target="_blank" title="twitter"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
+                        <a href="https://www.linkedin.com/company/endava" rel="me" itemProp="sameAs" aria-label="visit us on " target="_blank" title="linkedin"> <FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+                        <a href="https://www.instagram.com/endava/" rel="me" itemProp="sameAs" aria-label="visit us on " target="_blank" title="instagram"> <FontAwesomeIcon icon={['fab', 'instagram']} /></a>
                 </div>
         
                 <div className="pull-left float-left mr-3">
